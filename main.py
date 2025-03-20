fae03276cef84932e43a3fd539b8a189a22d3acb
Suresh S P <suresh.sp@mathco.com>
2025-03-20 18:19:17 +0530

    defining methods inside class
    calculating the total price

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 class Item:
-    pass
+    def calculate_total_price(self,x,y):
+        return x*y
 
 item1 = Item()
-
 item1.name="phone"
 item1.price=500
 item1.quantity=5
+print(item1.calculate_total_price(item1.price,item1.quantity))
 
-print(type(item1))
-print(type(item1.name))
-print(type(item1.name))
