9df69062187a15a4c3f3e4be777ff73bf89f5aa1
Suresh S P <suresh.sp@mathco.com>
2025-03-20 20:15:15 +0530

    modified class

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 class Item:
-    def calculate_total_price(self,x,y):
-        return x*y
+    # Constructor - self function for class , which passes the parameters 
+    def __init__(self,name:str,price:float,quantity:int):
 
-item1 = Item()
-item1.name="phone"
-item1.price=500
-item1.quantity=5
-print(item1.calculate_total_price(item1.price,item1.quantity))
+        # Add validation to recieve the arguments
+        assert price>=0,f"Price {price} must be equal to or greater than zero"
+        assert quantity>=0,f"Quantity {quantity} must be equal to or greater than zero"
 
+        #Assign to self object
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+    # Functions inside class are called methods
+    def calculate_total_price(self):
+        return self.price*self.quantity
+
+item1 = Item("phone",1000,3.0)
+item2 = Item("Laptop",500,-2)
+
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
